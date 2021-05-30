898967e41dd8eefc183146b3da0eceb4494e6bc0
Suman <sumanghosh740@gmail.com>
2021-05-30 20:27:51 +0530

    Question Service class implemented

---
+++ src/main/java/com/exam/service/impl/QuestionServiceImpl.java
@@ -0,0 +1,44 @@
+package com.exam.service.impl;
+
+import com.exam.model.exam.Question;
+import com.exam.model.exam.Quiz;
+import com.exam.repo.QuestionRepository;
+import com.exam.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class QuestionServiceImpl implements QuestionService {
+
+    @Autowired
+    QuestionRepository questionRepository;
+
+
+    @Override
+    public Question addQuestion(Question question) {
+        return this.questionRepository.save(question);
+    }
+
+    @Override
+    public Question updateQuestion(Question question) {
+        return this.questionRepository.save(question);
+    }
+
+    @Override
+    public Set<Question> getQuestion() {
+        return new HashSet<>(this.questionRepository.findAll());
+    }
+
+    @Override
+    public Question getQuestion(Long questionId) {
+        return this.questionRepository.findById(questionId).get();
+    }
+
+    @Override
+    public Set<Question> getQuestionOfQuiz(Quiz quiz) {
+        return this.questionRepository.findByQuiz(quiz);
+    }
+}
