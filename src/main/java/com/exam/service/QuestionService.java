023bd571f4a5ed117895be6be4c6f2601c192438
Suman <sumanghosh740@gmail.com>
2021-05-30 20:26:22 +0530

    quiz repo added

---
+++ src/main/java/com/exam/service/QuestionService.java
@@ -0,0 +1,19 @@
+package com.exam.service;
+
+import com.exam.model.exam.Question;
+import com.exam.model.exam.Quiz;
+
+import java.util.Set;
+
+public interface QuestionService {
+
+    public Question addQuestion(Question question);
+
+    public Question updateQuestion(Question question);
+
+    public Set<Question> getQuestion();
+
+    public Question getQuestion(Long questionId);
+
+    public Set<Question> getQuestionOfQuiz(Quiz quiz);
+}
---
+++ src/main/java/com/exam/service/QuizService.java
@@ -0,0 +1,17 @@
+package com.exam.service;
+
+import com.exam.model.exam.Quiz;
+
+import java.util.Set;
+
+public interface QuizService {
+    public Quiz addQuiz(Quiz quiz);
+
+    public Quiz updateQuiz(Quiz quiz);
+
+    public Set<Quiz> getQuizzes();
+
+    public Quiz getQuiz(Long quizId);
+
+    public void deleteQuiz(Long quizId);
+}
